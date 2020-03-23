916acaf2c06063234988979ed253616185a61bf3
sekrittt <61773395+sekrittt@users.noreply.github.com>
2020-03-23 13:01:13 +0500

    Update search.js

--- js/search.js
+++ js/search.js
@@ -72,7 +72,7 @@ function search(task) {
         window.open("https://ru.wikipedia.org/w/index.php?title=%D0%A1%D0%BB%D1%83%D0%B6%D0%B5%D0%B1%D0%BD%D0%B0%D1%8F%3A%D0%9F%D0%BE%D0%B8%D1%81%D0%BA&go=%D0%9F%D0%B5%D1%80%D0%B5%D0%B9%D1%82%D0%B8&ns0=1&search="+task);
         answer = "Уже ищю";
     } else {
-        break;
+        answer = "Я вас не понял";
     }
     return answer;
 }
