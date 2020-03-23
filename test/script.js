9ab7100b173cd0e89fe5eb0b8fa0a8ad2ac25f47
sekrittt <61773395+sekrittt@users.noreply.github.com>
2020-03-23 14:10:16 +0500

    Update script.js

--- test/script.js
+++ test/script.js
@@ -14,7 +14,6 @@ function startVideo() {
         err => console.error(err)
     )
 }
-startVideo()
 video.addEventListener('play', ()=>{
     const canvas = faceapi.createCanvasFromMedia(video)
     document.body.append(canvas)
