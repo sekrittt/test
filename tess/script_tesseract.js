ea11d1481de36f6f86c1a9027adba1a026914843
sekrittt <sekrittt@gmail.com>
2020-03-31 12:40:50 +0500

    Initial commit

--- tess/script_tesseract.js
+++ tess/script_tesseract.js
@@ -1,25 +1,24 @@
-// document.getElementById("ocr_url").value = ""; // Сбрасываем форму после перезагрузки
-// var control = document.getElementById("ocr_url");
-// control.addEventListener("change", function() {
-//     var files = control.files;
-//     document.getElementById("ocr_button").addEventListener("click", function() {
-//         document.getElementById("ocr_result").innerHTML  = "Идет распознавание текста...";
-//         Tesseract.recognize(files[0].name).then(function(result) {
-//             console.log(document.getElementById("sel").value);
-//             lang: document.getElementById("sel").value; // Язык текста
-//         }).then(function(result) {
-//             document.getElementById("ocr_result").innerHTML  = result.text;
-//         });
-//     });
-// });
 document.getElementById("ocr_url").value = ""; // Сбрасываем форму после перезагрузки
 var control = document.getElementById("ocr_url");
 control.addEventListener("change", function() {
-	var files = control.files;
-	document.getElementById("ocr_button").addEventListener("click", function() {
-		document.getElementById("ocr_result").innerHTML  = "Идет распознавание текста...";
-		Tesseract.recognize(files[0].name).then(function(result) {
-		    document.getElementById("ocr_result").innerHTML  = result.text; 
-	}   );
-	});
+    var files = control.files;
+    document.getElementById("ocr_button").addEventListener("click", function() {
+        document.getElementById("ocr_result").innerHTML  = "Идет распознавание текста...";
+        Tesseract.recognize(files[0].name).then(function(result) {
+            lang: "rus"; // Язык текста
+        }).then(function(result) {
+            document.getElementById("ocr_result").innerHTML  = result.text;
+        });
+    });
 });
+// document.getElementById("ocr_url").value = ""; // Сбрасываем форму после перезагрузки
+// var control = document.getElementById("ocr_url");
+// control.addEventListener("change", function() {
+// 	var files = control.files;
+// 	document.getElementById("ocr_button").addEventListener("click", function() {
+// 		document.getElementById("ocr_result").innerHTML  = "Идет распознавание текста...";
+// 		Tesseract.recognize(files[0].name).then(function(result) {
+// 		    document.getElementById("ocr_result").innerHTML  = result.text; 
+// 	}   );
+// 	});
+// });
