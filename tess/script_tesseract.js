9486eeb501f0bb2e53beb67d9f96cacf3ca02da2
sekrittt <sekrittt@gmail.com>
2020-03-31 12:24:23 +0500

    Initial commit

--- tess/script_tesseract.js
+++ tess/script_tesseract.js
@@ -5,6 +5,7 @@ control.addEventListener("change", function() {
     document.getElementById("ocr_button").addEventListener("click", function() {
         document.getElementById("ocr_result").innerHTML  = "Идет распознавание текста...";
         Tesseract.recognize(files[0].name).then(function(result) {
+            console.log(document.getElementById("sel").value);
             lang: document.getElementById("sel").value; // Язык текста
         }).then(function(result) {
             document.getElementById("ocr_result").innerHTML  = result.text;
